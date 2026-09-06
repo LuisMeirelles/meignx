65a65c9ce0417912b111bfd62b8bdedbca7331f4
Luis Meirelles <luis.gustavo.souto.meirelles@gmail.com>
2026-09-06 06:13:36 -0300

    send REQUEST_METHOD=GET via FCGI_PARAMS

--- meignx/src/main.c
+++ meignx/src/main.c
@@ -36,6 +36,50 @@ static void begin_request(const int fd)
     }
 }
 
+static void send_get_method(const int fd)
+{
+    typedef struct
+    {
+        unsigned char nameLengthB0;
+        unsigned char valueLengthB0;
+        unsigned char nameData[14];
+        unsigned char valueData[3];
+    } FCGI_NameValuePair;
+
+    typedef struct
+    {
+        FCGI_Header header;
+        FCGI_NameValuePair body;
+    } FCGI_ParamsRecord;
+
+    const FCGI_ParamsRecord record = {
+        .header = {
+            .version = 1,
+            .type = FCGI_PARAMS,
+            .requestIdB1 = 0,
+            .requestIdB0 = 1,
+            .contentLengthB1 = 0,
+            .contentLengthB0 = 19,
+            .paddingLength = 0,
+            .reserved = 0,
+        },
+        .body = {
+            .nameLengthB0 = 14,
+            .valueLengthB0 = 3,
+            .nameData = "REQUEST_METHOD",
+            .valueData = "GET",
+        },
+    };
+
+    const ssize_t success = send(fd, &record, sizeof(record), 0);
+
+    if (success == -1)
+    {
+        perror("send");
+        exit(errno);
+    }
+}
+
 int main()
 {
     const int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -58,7 +102,9 @@ int main()
 
     begin_request(fd);
 
-    printf("funcionou\n");
+    send_get_method(fd);
+
+    printf("funcionou");
 
     close(fd);
 
