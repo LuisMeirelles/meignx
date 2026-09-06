a900eadd190a775f04f212c245d6358d596e1aaf
Luis Meirelles <luis.gustavo.souto.meirelles@gmail.com>
2026-09-06 05:26:06 -0300

    send begin request

--- meignx/src/main.c
+++ meignx/src/main.c
@@ -1,31 +1,66 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include "fcgi.h"
 
-void begin_request()
+static void begin_request(const int fd)
 {
+    const FCGI_BeginRequestRecord record = {
+        .header = {
+            .version = 1,
+            .type = FCGI_BEGIN_REQUEST,
+            .requestIdB1 = 0,
+            .requestIdB0 = 1,
+            .contentLengthB1 = 0,
+            .contentLengthB0 = 8,
+            .paddingLength = 0,
+            .reserved = 0,
+        },
+        .body = {
+            .roleB1 = FCGI_RESPONDER >> 8,
+            .roleB0 = FCGI_RESPONDER & 0xFF,
+            .flags = 0,
+            .reserved = 0,
+        }
+    };
+
+    const ssize_t success = send(fd, &record, sizeof(record), 0);
+
+    if (success == -1)
+    {
+        perror("send");
+        exit(errno);
+    }
 }
 
 int main()
 {
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
+    const int fd = socket(AF_INET, SOCK_STREAM, 0);
 
-  struct sockaddr_in addr = {
-    .sin_family = AF_INET,
-    .sin_port = htons(9000),
-    .sin_addr = {
-      .s_addr = 0x220A8C0,
-    },
-  };
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(9000),
+        .sin_addr = {
+            .s_addr = 0x220A8C0,
+        },
+    };
 
-  int error_connect = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+    const int error_connect = connect(fd, (struct sockaddr*)&addr, sizeof(addr));
 
-  if (error_connect != 0) {
-    perror("connect");
-    return 1;
-  }
+    if (error_connect != 0)
+    {
+        perror("connect");
+        return errno;
+    }
 
-  printf("funcionou\n");
+    begin_request(fd);
 
-  return 0;
+    printf("funcionou\n");
+
+    close(fd);
+
+    return 0;
 }
